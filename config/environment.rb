02f4be855696144306993ae454e3902e917dfde5
Jaap van der Meer <jaapvandermeer@gmail.com>
2009-12-29 02:10:17 +0100

    Added some stuff

--- config/environment.rb
+++ config/environment.rb
@@ -38,6 +38,11 @@ Rails::Initializer.run do |config|
   config.gem 'RedCloth',     :version => '>= 4.0.0',   :lib => 'redcloth'
   config.gem 'aws-s3', :lib => 'aws/s3'
   
+  
+  
+  config.action_controller.session = { :key => "asdasdasd", :secret => "some secretasdasd phrase" }
+  
+  
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
